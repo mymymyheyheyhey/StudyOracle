06e62e9e55e33e7a59f087d7bff93166d9d13517
mymymyheyheyhey <97490838+mymymyheyheyhey@users.noreply.github.com>
2022-01-12 13:10:46 +0900

    2일차 README.md 추가

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # StudyOracle
 Oracle 학습 리포지토리
 
-### 1일차
+### 1일차 학습
+- Oracle 소개 및 설치
+- SQL Developer 설치와 사용법 학습
+- SQL 기본 SELECT 문 학습
 
-SQL 기본 SELECT 문 학습
-
+### 2일차 학습
+- SELECT 응용
+- WHERE 절
+- Alias
+- 사칙연산, 등가조건
+- IN 절
+- BETWEEN AND
+- LIKE 문
 
---
+++ day2/SQL 실습.sql
@@ -0,0 +1,57 @@
+-- 행 단위로 조회하는 셀렉션
+SELECT * FROM emp
+WHERE sal = 5000;
+
+SELECT * FROM emp
+WHERE job = 'CLERK';
+
+SELECT * FROM emp
+WHERE comm = 0 or comm IS NULL;
+
+-- WHERE AND 
+SELECT * FROM emp
+WHERE comm IS NULL AND job = 'ANALYST';
+
+SELECT empno, ename, deptno
+FROM emp
+WHERE deptno = 30;
+
+-- JOIN 2개 이상의 테이블을 하나의 테이블처럼 조회
+SELECT e.empno, e.ename, e.job, e.hiredate, e.sal, d.dname
+FROM emp e
+JOIN dept d 
+ON e.deptno = d.deptno;
+
+
+-- DISTINCT 복습
+SELECT job FROM emp;
+
+-- 별칭 ALIAS
+SELECT ename, job, sal * 12 AS ANNSAL
+FROM emp
+ORDER BY sal DESC; --ASC / DESC
+
+--WHERE
+SELECT ename, job, sal, sal * 12 AS ANNSAL
+FROM emp
+WHERE sal * 12 > 10000 
+ORDER BY sal * 12 DESC;
+
+-- IN
+SELECT ename, job, sal, sal * 12 AS ANNSAL
+FROM emp
+WHERE sal IN (800, 1600, 5000);
+
+-- BETWEEN a AND b
+SELECT ename, job, sal, sal * 12 AS ANNSAL
+FROM emp
+WHERE sal >= 1600 AND sal <= 2975;
+
+SELECT ename, job, sal, sal * 12 AS ANNSAL
+FROM emp
+WHERE sal BETWEEN 1600 AND 2975;
+
+--LIKE
+SELECT ename, job, sal, sal * 12 AS ANNSAL
+FROM emp
+WHERE ename LIKE '__RD'; --%, __RD
